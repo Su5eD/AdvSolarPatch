56fec066f608486d074a2d67176dbef1eeabe107
Su5eD <adamko.zachar@gmail.com>
2020-08-16 15:47:22 +0200

    update mod name

--- src/main/java/mods/su5ed/advsolarpatch/AdvSolarPatch.java
+++ src/main/java/mods/su5ed/advsolarpatch/AdvSolarPatch.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 public class AdvSolarPatch
 {
     public static final String MODID = "advsolarpatch";
-    public static final String NAME = "Advanced Solars Patch";
+    public static final String NAME = "Advanced Solar Machines Patcher";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
