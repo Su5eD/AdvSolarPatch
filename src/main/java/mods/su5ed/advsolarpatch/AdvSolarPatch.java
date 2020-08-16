6c107a381fd6651a488bb59ab0ae00035e2fb572
Su5eD <adamko.zachar@gmail.com>
2020-08-16 15:52:41 +0200

    update version to 1.1

--- src/main/java/mods/su5ed/advsolarpatch/AdvSolarPatch.java
+++ src/main/java/mods/su5ed/advsolarpatch/AdvSolarPatch.java
@@ -11,7 +11,7 @@ public class AdvSolarPatch
 {
     public static final String MODID = "advsolarpatch";
     public static final String NAME = "Advanced Solar Panels Patcher";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     private static Logger logger;
 
